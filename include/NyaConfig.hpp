a7487362dba7132d22bb7a74dcd8d284c1e5b011
Alex Uskov <FrozenAlex@users.noreply.github.com>
2022-04-22 00:38:17 +0300

    Reorder settings

--- include/NyaConfig.hpp
+++ include/NyaConfig.hpp
@@ -34,28 +34,19 @@ DECLARE_CONFIG(NyaConfig,
     CONFIG_VALUE(NSFWEnabled, bool, "NSFW Enabled", false);
     CONFIG_VALUE(SkipNSFWWarning, bool, "Skip NSFW warning", false);
     
-    CONFIG_VALUE(pausePosition, UnityEngine::Vector3, "Pause Position", UnityEngine::Vector3(-1.648f, 1.516, 0.916f));
+    CONFIG_VALUE(pausePosition, UnityEngine::Vector3, "Pause Position", UnityEngine::Vector3(-1.648f, 1.516f, 0.916f));
     CONFIG_VALUE(pauseRotation, UnityEngine::Vector3, "Pause Rotation", UnityEngine::Vector3(1.120f, 286.746f, 2.421f));
 
-    CONFIG_VALUE(gamePosition, UnityEngine::Vector3, "Game Position", UnityEngine::Vector3(-1.648f, 1.516, 0.916f));
+    CONFIG_VALUE(gamePosition, UnityEngine::Vector3, "Game Position", UnityEngine::Vector3(-1.648f, 1.516f, 0.916f));
     CONFIG_VALUE(gameRotation, UnityEngine::Vector3, "Game Rotation", UnityEngine::Vector3(1.120f, 286.746f, 2.421f));
 
-    CONFIG_VALUE(menuPosition, UnityEngine::Vector3, "Menu Position", UnityEngine::Vector3(-1.648f, 1.516, 0.916f));
+    CONFIG_VALUE(menuPosition, UnityEngine::Vector3, "Menu Position", UnityEngine::Vector3(-1.648f, 1.516f, 0.916f));
     CONFIG_VALUE(menuRotation, UnityEngine::Vector3, "Menu Rotation", UnityEngine::Vector3(1.120f, 286.746f, 2.421f));
 
     CONFIG_VALUE(resultPosition, UnityEngine::Vector3, "Result Position", UnityEngine::Vector3(2.321f, 2.883f, 3.851f));
     CONFIG_VALUE(resultRotation, UnityEngine::Vector3, "Result Rotation", UnityEngine::Vector3(338.810f, 30.401f, 359.280f));
 
     CONFIG_INIT_FUNCTION(
-        CONFIG_INIT_VALUE(pausePosition);
-        CONFIG_INIT_VALUE(pauseRotation);
-        CONFIG_INIT_VALUE(gamePosition);
-        CONFIG_INIT_VALUE(gameRotation);
-        CONFIG_INIT_VALUE(menuPosition);
-        CONFIG_INIT_VALUE(menuRotation);
-        CONFIG_INIT_VALUE(resultPosition);
-        CONFIG_INIT_VALUE(resultRotation);
-
         CONFIG_INIT_VALUE(inPause);
         CONFIG_INIT_VALUE(inResults);
         CONFIG_INIT_VALUE(inGame);
@@ -69,5 +60,14 @@ DECLARE_CONFIG(NyaConfig,
         CONFIG_INIT_VALUE(AutoNyaDelay);
         CONFIG_INIT_VALUE(NSFWEnabled);
         CONFIG_INIT_VALUE(SkipNSFWWarning);
+
+        CONFIG_INIT_VALUE(pausePosition);
+        CONFIG_INIT_VALUE(pauseRotation);
+        CONFIG_INIT_VALUE(gamePosition);
+        CONFIG_INIT_VALUE(gameRotation);
+        CONFIG_INIT_VALUE(menuPosition);
+        CONFIG_INIT_VALUE(menuRotation);
+        CONFIG_INIT_VALUE(resultPosition);
+        CONFIG_INIT_VALUE(resultRotation);
     )
 )
