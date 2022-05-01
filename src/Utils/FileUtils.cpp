112c83a9203792ab28158bb40d589cb456e1ee03
Alex Uskov <FrozenAlex@users.noreply.github.com>
2022-05-01 23:29:30 +0300

    Make file matching case insensitive

--- src/Utils/FileUtils.cpp
+++ src/Utils/FileUtils.cpp
@@ -27,11 +27,37 @@ namespace FileUtils {
         return strings;
     }
 
+
+    /*
+    * Find Case Insensitive Sub String in a given substring
+    */
+    size_t findCaseInsensitive(std::string data, std::string toSearch, size_t pos = 0)
+    {
+        // Convert complete given String to lower case
+        std::transform(data.begin(), data.end(), data.begin(), ::tolower);
+        // Convert complete given Sub String to lower case
+        std::transform(toSearch.begin(), toSearch.end(), toSearch.begin(), ::tolower);
+        // Find sub string in given string
+        return data.find(toSearch, pos);
+    }
+
+    // Gets all supported files
     std::vector<std::string> getAllFilesInFolder(const std::string& path){
          std::vector<std::string> strings;
         for (const auto & entry : std::filesystem::directory_iterator(path)) {
             if (entry.is_regular_file()) {
-                strings.push_back(entry.path());
+                if (
+                    findCaseInsensitive(entry.path(), ".png") != std::string::npos ||
+                    findCaseInsensitive(entry.path(), ".jpg") != std::string::npos ||
+                    findCaseInsensitive(entry.path(), ".jpeg") != std::string::npos ||
+                    findCaseInsensitive(entry.path(), ".webp") != std::string::npos ||
+                    findCaseInsensitive(entry.path(), ".tiff") != std::string::npos ||
+                    findCaseInsensitive(entry.path(), ".bmp") != std::string::npos ||
+                    findCaseInsensitive(entry.path(), ".gif") != std::string::npos
+                ) {
+                    strings.push_back(entry.path());
+                } 
+               
             }
         }
         return strings;
