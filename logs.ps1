7503ae5016b8bf6eb97c5828cfe0a08a30fbe7ca
Alex Uskov <FrozenAlex@users.noreply.github.com>
2023-04-27 08:28:35 +0400

    Add adb pull to get tombstones as well

--- logs.ps1
+++ logs.ps1
@@ -11,4 +11,10 @@ if (Test-Path ./logs/) {
 
 
 adb pull "/storage/emulated/0/Android/data/com.beatgames.beatsaber/files/logs" ./logs/
+
+adb pull "/storage/emulated/0/Android/data/com.beatgames.beatsaber/files/tombstone_00" ./logs/
+adb pull "/storage/emulated/0/Android/data/com.beatgames.beatsaber/files/tombstone_01" ./logs/
+adb pull "/storage/emulated/0/Android/data/com.beatgames.beatsaber/files/tombstone_02" ./logs/
+
+
 adb pull "/storage/emulated/0/ModData/com.beatgames.beatsaber/Configs/Nya.json" ./logs/
