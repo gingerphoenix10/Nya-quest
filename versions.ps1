cd4f2e80c2e0b10af24762a7498f7486485a1584
Alex Uskov <FrozenAlex@users.noreply.github.com>
2023-06-05 10:16:12 +0400

    Add release scripts

---
+++ versions.ps1
@@ -0,0 +1,18 @@
+# File with versions of beat saber to build for mapped to branches
+# If releasing for new version of beat saber, add new version to the end of the list and add new branch to branches list
+
+# Branches to get version from
+$branches = @(
+    'bs-v1.24.0',
+    'bs-v1.25.1',
+    'bs-v1.27.0',
+    'bs-1.28.0'
+)
+
+# Versions of beat saber 1:1 with tags
+$versions = @(
+    '1.24.0',
+    '1.25.1',
+    '1.27.0',
+    '1.28.0'
+)
